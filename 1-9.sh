7570ecabb36cda0a04659b4ee61bb4fcbd5ea157
ThisisFinee <108457032+ThisisFinee@users.noreply.github.com>
2023-06-01 13:34:43 +0300

    End

--- 1-9.sh
+++ 1-9.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
+# Get variables:
 x=$1
 y=$2
+# Cheking variables(numbers or not?):
 if [[ $x =~ ^[+,-]?[0-9]+$ && $y =~ ^[+,-]?[0-9]+$ ]]; then
-	a=$(( $x*2 + $y*2 ))
+	a=$(( $x*2 + $y*2 )) #Perimeter 
+	#Cheking negative numbers
 	if [[ $x =~ ^[-][0-9]+$ || $y =~ ^[-][0-9]+$ ]]; then
+		# if negative number:
 		echo "Data entered incorrectly!!!(x<0 or y<0)"
 	else
+		# if pozitive number:
 		echo "Perimeter of the rectangle is equal: " $a
 	fi
 else
+	#if variables are not numbers:
 	echo "Variables are not numbers!!!"
 fi
